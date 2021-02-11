b6a09597a06129604b66420b17a34ed71ff0ead1
Moenupa <mineworldy@outlook.com>
2021-02-11 17:07:23 +0800

    modified:   startup.sh

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,12 +2,32 @@
 
 ##/etc/rc.local.d/
 # Create Start-up Service for sh script under /etc/rc.local.d/
-sudo touch /etc/systemd/system/rc-local.service
-sudo printf "[Unit]\nDescription="/etc/rc.local Compatibility"\n[Service]\nType=oneshot\nExecStart=/etc/rc.local start\nTimeoutSec=0\nStandardInput=tty\nRemainAfterExit=yes\nSysVStartPriority=99\n[Install]\nWantedBy=multi-user.target\n" > /etc/systemd/system/rc-local.service
+sudo touch "/etc/systemd/system/rc-local.service"
+sudo cat > "/etc/systemd/system/rc-local.service" << EOF
+[Unit]
+Description="/etc/rc.local Compatibility"
+[Service]
+Type=oneshot
+ExecStart=/etc/rc.local start
+TimeoutSec=0
+StandardInput=tty
+RemainAfterExit=yes
+SysVStartPriority=99
+[Install]
+WantedBy=multi-user.target
+EOF
 ## Link to Shell Script
-sudo touch /etc/rc.local
-sudo printf "#!/bin/sh\nif test -d /etc/rc.local.d; then\n\tfor rcscript in /etc/rc.local.d/\*.sh; do test -r "${rcscript}" && sh ${rcscript}; done\n\tunset rcscript\nfi\n" > /etc/rc.local
+sudo touch "/etc/rc.local"
+sudo cat > "/etc/rc.local" << EOF 
+#!/bin/sh
+if test -d /etc/rc.local.d; then
+	for rcscript in /etc/rc.local.d/\*.sh; do
+		test -r "${rcscript}" && sh ${rcscript}
+	done
+	unset rcscript
+fi
+EOF
 ## Enable the service
-sudo chmod 755 /etc/rc.local
-sudo mkdir /etc/rc.local.d
+sudo chmod 755 "/etc/rc.local"
+sudo mkdir "/etc/rc.local.d"
 sudo systemctl enable rc-local.service
